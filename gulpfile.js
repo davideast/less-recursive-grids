8a68013e719d07d84eadc6d0fc4bc6116520d8e6
David <david@firebase.com>
2014-08-25 21:31:45 -0700

    Make it so.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+  less = require('gulp-less'),
+  connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('less', function () {
+  gulp.src('./src/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+  gulp.src('./dist/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./*.html'], ['html']);
+  gulp.watch(['./src/*.less'], ['less']);
+  gulp.watch(['./dist/*.css'], ['css']);
+});
+
+
+gulp.task('default', ['connect', 'watch']);
